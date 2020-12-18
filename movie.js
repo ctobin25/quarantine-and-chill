fbb3757c51f9feacf0e840dde50b5e62369472ce
Grant <beauhengst@gmail.com>
2020-12-17 22:20:00 -0500

    checked api functionality and wrote what needs to be done, created variables

--- movie.js
+++ movie.js
@@ -1,15 +1,39 @@
-	var movie = $(this).attr("data-name");
-	var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+	//set up variables
+	//working url= https://www.omdbapi.com/?t=Forrest%20Gump&apikey=trilogy
+	
+	var queryURL = "https://www.omdbapi.com/?t="  + authKey;
+	var authKey = "&apikey=trilogy";
 
-	$.ajax({
-	  url: queryURL,
-	  method: "GET"
-	}).then(function(response) {
-	  $("#movies-view").text(JSON.stringify(response));
-	});
-
-	alert(movieName)
-  }
+	var movieLength  = "";
+	var movieGenre   = "";
+	var movieCountry = "";
 
 
 
+
+	//functions
+	function runQuery(queryURL){
+
+		$.ajax({url: queryURL, Method: "GET"})
+			.done(function(MovieData) {
+				console.log(MovieData)
+			})
+
+			
+	}
+
+
+	//main proceccess 
+	$('#searchBTN').on('click', function(){
+		
+		runQuery("https://www.omdbapi.com/?t=Forrest%20Gump&apikey=trilogy" );
+
+		return false;
+	})
+
+	//1. Retrieve User inputs and convert to variables
+	//2. Use those variables to run an ajax call to the OMDBapi
+	//3 Break Down the OMDbai object into useable fields
+	//4. Dynamically Generate html content
+
+	//5. Dealing with bugs or situations
